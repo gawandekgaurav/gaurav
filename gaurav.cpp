87b8e9790e10d37559155d43bb143fe85a64a164
Gaurav Gawande <121332976+gawandekgaurav@users.noreply.github.com>
2024-04-26 00:38:01 +0530

    Create gaurav.cpp

---
+++ gaurav.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	cout<<"Welcome to c++";
+	int no1=100;
+	cout<<"no1"<<no1<<endl;;
+	cout<<"enter no1"<<endl;
+	cin>>no1;
+	cout<<"No1"<<no1<<endl;
+	
+	
+	cout<<sizeof(no1)<<endl;
+	cout<<sizeof(char)<<endl;
+	cout<<sizeof(double)<<endl;
+}
